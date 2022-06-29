298e13d6f7a7dff1af9435cd3e636b9a749dfc70
kdh4365 <kdh4365@gmail.com>
2022-06-29 17:02:41 +0900

    데이터 목록조회

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,5 +1,4 @@
 package com.example.firstproject.controller;
-
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
@@ -11,18 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @Slf4j //로깅을 위한 골뱅이(어노테이션)
 public class ArticleController {
-
     @Autowired // 스프링 부트가 미리 생성해 좋은 객체를 가져다가 자동 연결!
     private ArticleRepository articleRepository;
-
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
     }
-
     @PostMapping("/articles/creates")
     public String createArticle(ArticleForm form) {
         log.info(form.toString());
@@ -31,11 +30,10 @@ public class ArticleController {
         // 1. Dto를 변환! Entity!
         Article article = form.toEntity();
         log.info(article.toString());
-        //        System.out.println(article.toString());
-
         // 2. Repository에게 Entity를 DB안에 저장하게 함!
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
+//        System.out.println(saved.toString());
 //      System.out.println(saved.toString());
 
         return "";
@@ -53,5 +51,17 @@ public class ArticleController {
 
         // 3: 보여줄 페이지를 설정!
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1: 모든 Article을 가져온다!
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        // 2: 가져온 Article 묶음을 뷰로 전달!
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3: 뷰 페이지를 설정!
+        return"articles/index"; //articles/index.mustache
     }
 }
